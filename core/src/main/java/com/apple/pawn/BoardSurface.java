fdbe2064859988ed4cd81f9ea740dbddb7c6e8ef
k.k.apple0430 <k.k.apple0430@gmail.com>
2022-02-16 11:31:32 +0900

    仮のマップ作製

--- core/src/main/java/com/apple/pawn/BoardSurface.java
+++ core/src/main/java/com/apple/pawn/BoardSurface.java
@@ -1,5 +1,6 @@
 package com.apple.pawn;
 
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -9,6 +10,7 @@ import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.utils.Array;
 
 public class BoardSurface {
+    public static int MAP_WIDTH = 2048;
     private Array<Piece> aPiece;
     private Array<Square> aSquare;
 
@@ -16,7 +18,11 @@ public class BoardSurface {
     private Group group;
 
     public BoardSurface() {
-        img = new Texture("badlogic.jpg");
+        Pixmap mapPix = new Pixmap(MAP_WIDTH, MAP_WIDTH, Pixmap.Format.RGB888);
+        mapPix.setColor(0, 1, 0, 1);
+        mapPix.fill();
+        img = new Texture(mapPix);
+        mapPix.dispose();
         aPiece = new Array<Piece>();
         aSquare = new Array<Square>();
         group = new Group();
@@ -30,6 +36,20 @@ public class BoardSurface {
         pi = new Piece("2P");
         pi.setSquareNo(1);
         aPiece.add( pi );
+
+        int i;
+        int j;
+        for(i = 0; i <= 4; i += 4) {
+            for(j = 0; j < 8; j++) newSquare(j, i);
+            newSquare(7, 1+i);
+            for(j = 7; j >= 0; j--) newSquare(j, 2+i);
+            newSquare(0, 3+i);
+        }
+    }
+
+    private void newSquare(int x, int y) {
+        Square sq = new Square(x, y);
+        aSquare.add(sq);
     }
 
     public void update() {
@@ -41,12 +61,16 @@ public class BoardSurface {
     public void draw (Batch batch, ShapeRenderer renderer) {
         batch.begin();
         Sprite sprite = new Sprite( new TextureRegion(img));
-        sprite.setSize(2000, 2000);
+        sprite.setSize(MAP_WIDTH, MAP_WIDTH);
 //        sprite.setRegion(100,100,100,100);
         sprite.setPosition(0, 0);   // ※仮
         sprite.flip(false, true);       // 上下反転 setRegionより後に
         sprite.draw(batch);
         batch.end();
+
+        for(Square sq : aSquare) {
+            sq.draw(batch, renderer);
+        }
 
         for(Piece pi : aPiece) {
             pi.draw(batch, renderer);
--- core/src/main/java/com/apple/pawn/Square.java
+++ core/src/main/java/com/apple/pawn/Square.java
@@ -4,17 +4,27 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Square {
+    public static int SQUARE_LENGTH = 256;
+
     protected int type;               // マスの種類
     protected String document;        // マスの文字
 
-    public Square( String doc ) {
-        document = doc;
+    protected int x;
+    protected int y;
+
+    public Square(int xPos, int yPos) {
+        x = xPos;
+        y = yPos;
     }
 
     public void update() {
     }
 
     public void draw (Batch batch, ShapeRenderer renderer) {
+        renderer.begin(ShapeRenderer.ShapeType.Filled);
+        renderer.setColor(1, 1, 1, 1);
+        renderer.box(x*SQUARE_LENGTH+1, y*SQUARE_LENGTH+1, 0, SQUARE_LENGTH-2, SQUARE_LENGTH-2, 0);
+        renderer.end();
     }
 
     public void dispose () {

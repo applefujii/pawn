a7776c9de825930bf27837a344239b7984cb6281
y.nozato <appleumeda.nozato@gmail.com>
2022-05-12 10:12:39 +0900

    微修正

--- core/src/main/java/com/apple/pawn/Result.java
+++ core/src/main/java/com/apple/pawn/Result.java
@@ -2,6 +2,7 @@ package com.apple.pawn;
 
 import android.support.annotation.NonNull;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;

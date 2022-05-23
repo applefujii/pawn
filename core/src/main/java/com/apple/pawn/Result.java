be2122b3af5c5905fa063ce65a8ce9d8b46935ab
y.nozato <appleumeda.nozato@gmail.com>
2022-05-23 11:45:34 +0900

    【リザルト画面】配列管理と一部の座標修正

--- core/src/main/java/com/apple/pawn/Result.java
+++ core/src/main/java/com/apple/pawn/Result.java
@@ -11,7 +11,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.Array;
 
 public class Result extends UIParts {
-    private static final int INDEX_WIDTH = 120, INDEX_HEIGHT = 154;
+    private static final int INDEX_WIDTH = 120, INDEX_HEIGHT = 150;
     public static final int SQUARE_WIDTH = 50, SQUARE_HEIGHT = 50;
     private static final Array<String> TYPE_STR, TYPE_STR_JP;
 
@@ -21,6 +21,8 @@ public class Result extends UIParts {
     private final float spy;
     private final float pax,pay;
     private final float spriteWidth;
+    private final int[] multx = {2,4,6};
+    private final String[] item = {"名前","ターン数","止まった回数"};
     private final UI ui;
     private final Array<Sprite> aSqSprite;
     private final Pawn game;
@@ -35,11 +37,13 @@ public class Result extends UIParts {
     public Result(String name, int x, int y, int width, int height, int group, PlayerManager playerManager, AssetManager manager,final Pawn game) {
         super(name, x, y, width, height, group);
         this.playerManager = playerManager;
+        //px = x;
+        py = 14;
         pax = px;
         pay = py;
         span = (float) (height - INDEX_HEIGHT - pay) / 6;
         spx = (float) (width - INDEX_WIDTH - pax) / 7;
-        spy = (float) height / 20;
+        spy = (float) height / 7; //20
         spriteWidth = span * ((float) Piece.WIDTH / Piece.HEIGHT);
         aSqSprite = new Array<>();
         this.game = game;
@@ -73,39 +77,40 @@ public class Result extends UIParts {
         batch.begin();
         font.getData().setScale(1);
 
-        PawnUtils.fontDrawXCenter(font, batch, "名前", spx*2, spy*2);
-        PawnUtils.fontDrawXCenter(font, batch, "ターン数", spx*4, spy*2);
-        PawnUtils.fontDrawXCenter(font, batch, "止まった回数", spx*6, spy*2);
+        for(int i=0; i<multx.length; i++){
+            PawnUtils.fontDrawXCenter(font, batch, item[i], spx * multx[i], spy);
+        }
+
         float w = spx*5;
+        float h = spy*2;
         for(String st : TYPE_STR_JP) {
-            PawnUtils.fontDrawXCenter(font, batch, st, w, spy*3);
+            PawnUtils.fontDrawXCenter(font, batch, st, w, h-SQUARE_HEIGHT);
             w += spx;
         }
         float g = INDEX_WIDTH + (spriteWidth / 2);
-        float h = spy*5;
         for(Player player : playerManager.getGoalPlayer()){
             Sprite sprite = player.getPiece().getSprite();
             sprite.setSize(spriteWidth, span);
             sprite.setCenter(g, h);
             sprite.draw(batch);
-            PawnUtils.fontDrawCenter(font, batch, player.getName(), spx*2, h);
-            PawnUtils.fontDrawCenter(font, batch, player.getGoalTurn()+"ターン", spx*4, h);
+            PawnUtils.fontDrawCenter(font, batch, player.getName(), spx * multx[0], h);
+            PawnUtils.fontDrawCenter(font, batch, player.getGoalTurn()+"ターン", spx * multx[1], h);
             w = spx*5;
             for(int i = 0; i < TYPE_STR.size; i++) {
                 Sprite sqSprite = aSqSprite.get(i);
                 sqSprite.setCenter(w, h);
                 sqSprite.draw(batch);
                 PawnUtils.fontDrawCenter(font, batch, String.valueOf(player.getAResultDetail().get(i)), w, h);
+                PawnUtils.fontDrawCenter(font, batch, "w="+w+",\nh="+h, w, h);
                 w += spx;
             }
-            h += spy*3;
+            h += spy;
         }
         batch.end();
         ui.add(new UIPartsSelect("title_link", 300, 300, 300, 20, 1, 0, true, "タイトルへ戻る"));
         ui.draw(batch, renderer, font, 1);
         int select = ui.getSelect();
-        if(FlagManagement.is(Flag.UI_INPUT_ENABLE)) ui.update();
-        Gdx.app.debug("fps", "select="+select);
+        //if(FlagManagement.is(Flag.UI_INPUT_ENABLE)) ui.update();
         if(select != -1 ) {
             game.setScreen(new TitleScreen(game));
         }
